0365e37776896133d6afffcc2c9161b26df0459d
cdpeterpan <cd.peter.pan@outlook.com>
2019-07-17 17:02:56 +0800

    add contract demo

--- sc-demo/simple.sol
+++ sc-demo/simple.sol
@@ -2,9 +2,9 @@ pragma solidity ^0.5.0;
 
 contract SimpleStorage {
     uint storedData;
-    address addr;
+
     function set(uint x) public {
-        storedData = x;
+        storedData += x;
     }
 
     function get() public view returns (uint) {
